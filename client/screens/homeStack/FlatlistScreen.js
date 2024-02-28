c50668df8bcb03b43d0854faf2570c4e50a033a7
Filbert Tansil <133459777+filberttansil@users.noreply.github.com>
2024-02-28 11:39:00 +0700

    feat: skeleton loader and infinite scroll
    Implemented infinite scroll for FlatListScreen.js and skeleton loader for loading animation

--- client/screens/homeStack/FlatlistScreen.js
+++ client/screens/homeStack/FlatlistScreen.js
@@ -13,19 +13,41 @@ import React, { useEffect, useLayoutEffect, useState } from "react";
 import { useNavigation } from "@react-navigation/native";
 import { SimpleLineIcons } from "@expo/vector-icons";
 import Button from "../../components/Button";
+
+import ContentLoader, { Rect, Circle } from "react-content-loader/native";
+
+const MyLoader = () => (
+  <ContentLoader
+    speed={1}
+    width={"100%"}
+    height={180}
+    viewBox="0 0 400 160"
+    backgroundColor="#d9d9d9"
+    foregroundColor="#ededed"
+  >
+    {/* First rectangle */}
+    <Rect x="20" y="15" rx="4" ry="4" width="360" height="50" />
+
+    {/* Second rectangle */}
+    <Rect x="20" y="80" rx="4" ry="4" width="360" height="50" />
+  </ContentLoader>
+);
+
 export default function FlatListScreen() {
   const { navigate, setOptions, openDrawer } = useNavigation();
   const [data, setData] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
   const [refreshing, setRefreshing] = useState(false);
+  const API = "https://jsonplaceholder.typicode.com/todos";
+  // State akan di set jika scroll sdh sampai end
+  const [page, setPage] = useState(1);
   const fetchData = async () => {
     try {
-      const response = await fetch(
-        "https://jsonplaceholder.typicode.com/todos"
-      );
+      setIsLoading(true);
+      const response = await fetch(API + `?_page=${page}_per_page=10`);
       if (!response.ok) throw new Error("Failed to fetch data");
-      const data = await response.json();
-      setData(data);
+      const responseJSON = await response.json();
+      setData([...data, ...responseJSON]);
       setIsLoading(false);
     } catch (error) {
       console.log(error);
@@ -34,12 +56,27 @@ export default function FlatListScreen() {
 
   const handleRefresh = () => {
     setRefreshing(true);
+    setData([]);
     fetchData();
     setRefreshing(false);
   };
+  const renderHeader = () => {
+    return (
+      <View>
+        <Text style={styles.headerAndFooter}>To Do List</Text>
+      </View>
+    );
+  };
+  const renderLoader = () => {
+    if (isLoading) {
+      return <MyLoader />;
+    } else return null;
+  };
+  const handleOnEndReached = () => setPage(page + 1);
+
   useEffect(() => {
     fetchData();
-  }, []);
+  }, [page]);
 
   useLayoutEffect(() => {
     setOptions({
@@ -51,13 +88,7 @@ export default function FlatListScreen() {
       ),
     });
   }, []);
-  if (isLoading) {
-    return (
-      <SafeAreaView style={styles.loadingContainer}>
-        <ActivityIndicator size={"large"} color={"lightblue"} />
-      </SafeAreaView>
-    );
-  }
+
   return (
     <SafeAreaView style={styles.safeView}>
       <View style={{ padding: 10 }}>
@@ -66,6 +97,7 @@ export default function FlatListScreen() {
           onPress={() => navigate("TodoFormScreen")}
         />
       </View>
+
       <FlatList
         data={data}
         renderItem={({ item }) => (
@@ -76,43 +108,39 @@ export default function FlatListScreen() {
             <Text>{item.title}</Text>
           </TouchableOpacity>
         )}
-        keyExtractor={(item) => item.id}
+        keyExtractor={(item, index) => index.toString()}
         // Props ini merender react component
         ItemSeparatorComponent={() => <View style={{ height: 16 }} />}
         // Props ini yang dirender jika data kosong
-        ListEmptyComponent={() => (
-          <View
-            style={{
-              flex: 1,
-              alignItems: "center",
-              justifyContent: "center",
-              marginTop: 100,
-            }}
-          >
-            <Image
-              source={require("../../assets/pikachu.png")}
-              style={{ height: 300, width: "100%" }}
-              resizeMode="contain"
-            />
-            <Text style={{ fontSize: 25, fontWeight: "bold" }}>
-              Sorry yee.. Tak ada data!
-            </Text>
-          </View>
-        )}
+        // ListEmptyComponent={() => (
+        //   <View
+        //     style={{
+        //       flex: 1,
+        //       alignItems: "center",
+        //       justifyContent: "center",
+        //       marginTop: 100,
+        //     }}
+        //   >
+        //     <Image
+        //       source={require("../../assets/pikachu.png")}
+        //       style={{ height: 300, width: "100%" }}
+        //       resizeMode="contain"
+        //     />
+        //     <Text style={{ fontSize: 25, fontWeight: "bold" }}>
+        //       Sorry yee.. Tak ada data!
+        //     </Text>
+        //   </View>
+        // )}
         // Props merender react element di awal list
-        ListHeaderComponent={() => (
-          <View>
-            <Text style={styles.headerAndFooter}>To Do List</Text>
-          </View>
-        )}
+        ListHeaderComponent={renderHeader}
         // Props ini merender react element di akhir list
-        ListFooterComponent={() => (
-          <Text style={styles.headerAndFooter}>End of To Do List</Text>
-        )}
+        ListFooterComponent={renderLoader}
         // Props untuk refresh menerima boolean
         refreshing={refreshing}
         // Menerima function, buat function sesuai dengan yang kamu mau
         onRefresh={handleRefresh}
+        onEndReached={handleOnEndReached}
+        onEndReachedThreshold={0}
       />
     </SafeAreaView>
   );
@@ -147,5 +175,6 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: "center",
     alignItems: "center",
+    padding: 15,
   },
 });
